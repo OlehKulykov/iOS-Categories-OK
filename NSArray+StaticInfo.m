18b33ed90860327a999ccf28728f59d65a41a257
oleh.kulykov <oleh.kulykov@masterofcode.com>
2012-12-21 17:38:18 +0200

    Prev and next array index from current

--- NSArray+StaticInfo.m
+++ NSArray+StaticInfo.m
@@ -16,12 +16,25 @@
 
 + (NSUInteger) array:(NSArray *)array previvousIndexFrom:(NSUInteger)index
 {
-	
+	if (index > 0 && index < NSNotFound)
+	{
+		const NSUInteger prevIndex = index - 1;
+		if (prevIndex < [array count])
+		{
+			return prevIndex;
+		}
+	}
+	return NSNotFound;
 }
 
 + (NSUInteger) array:(NSArray *)array nextIndexFrom:(NSUInteger)index
 {
-	
+	const NSUInteger nextIndex = index + 1;
+	if (nextIndex < [array count])
+	{
+		return nextIndex;
+	}
+	return NSNotFound;
 }
 
 @end
